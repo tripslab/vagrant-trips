8ff93c79e7c1b1e02be3b2f05fe4e0d65213d8fc
Ritwik Bose <mrmechko@gmail.com>
2018-09-18 14:54:53 -0400

    stop the provisioner from crashing if ~/.jupyter_mode doesn't exist

--- provisioners/jupyter.sh
+++ provisioners/jupyter.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 cat /home/vagrant/.jupyter_mode
 
-if [[ -f /home/vagrant/.jupyter_mode && "$(cat /home/vagrant/.jupyter_mode)" == "enabled" ]]
+if [ ! -e /home/vagrant/.jupyter_mode ] 
+then
+	echo "Jupyter mode is disabled.  Run \'echo enabled > /vagrant/home/.jupyter_mode\' to enable"
+	exit
+elif [[ -f /home/vagrant/.jupyter_mode && "$(cat /home/vagrant/.jupyter_mode)" == "enabled" ]]
 then
 	cd /home/vagrant/shared/notebooks
 	jupyter notebook --ip=0.0.0.0
 else
-	echo "Jupyter mode is disabled.  run \'echo enabled > /vagrant/home/.jupyter_mode\' to enable"
+	echo "Jupyter mode is disabled.  Run \'echo enabled > /vagrant/home/.jupyter_mode\' to enable"
 fi
