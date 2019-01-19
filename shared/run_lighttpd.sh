e2df3e2c7729eb37402cd0fe2d4b63cbc067e0ab
Rik <rbose@cs.rochester.edu>
2019-01-19 16:01:19 -0500

    woo it works.  use 'vagrant reload --provision-with server' to restart the vagrant machine with the WebParser running

--- shared/run_lighttpd.sh
+++ shared/run_lighttpd.sh
@@ -3,5 +3,5 @@
 mkdir -p logs; 
 
 # yeah you need to figure out how to kill this yourself, buddy
-/home/vagrant/shared/step/bin/trips-step -display None -logdir logs &
-lighttpd -D -f /home/vagrant/shared/lighttpd.conf &
+screen -S server -d -m lighttpd -D -f /home/vagrant/shared/lighttpd.conf
+screen -S step -d -m /home/vagrant/shared/step/bin/trips-step -display None -logdir logs 
