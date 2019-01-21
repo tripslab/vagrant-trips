a1d8a1fc0d37f8993d4fca61f4a55326a00e8e31
Rik <rbose@cs.rochester.edu>
2019-01-20 22:08:26 -0500

    logdir fix

--- shared/run_lighttpd.sh
+++ shared/run_lighttpd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir -p logs; 
+mkdir -p /home/vagrant/shared/logs; 
 
 # yeah you need to figure out how to kill this yourself, buddy
 screen -S server -d -m lighttpd -D -f /home/vagrant/shared/lighttpd.conf
-screen -S step -d -m /home/vagrant/shared/step/bin/trips-step -display None -logdir logs 
+screen -S step -d -m /home/vagrant/shared/step/bin/trips-step -display None -logdir /home/vagrant/shared/logs 
