f2b549dae34bb5907f2884695bf15cddade29367
Rik <rbose@cs.rochester.edu>
2019-06-20 00:51:34 -0400

    removed diesel from the box

--- provisioners/diesel.sh
+++ provisioners/diesel.sh
@@ -4,23 +4,27 @@
 # if you want to modify the code, clone the repos and install them into 
 # your python environment using `pip install .`
 
-pip install --upgrade git+git://github.com/mrmechko/diesel-python
+pip install --upgrade nltk
+python -m nltk.downloader wordnet
+python -m nltk.downloader stopwords
+
+#pip install --upgrade git+git://github.com/mrmechko/diesel-python
 
 echo "diesel-python installed"
 
 # genesis automatically installs spacy and a language model among other things
-pip install --upgrade git+git://github.com/mrmechko/genesis
+#pip install --upgrade git+git://github.com/mrmechko/genesis
 
 echo "genesis installed"
 
-python -m spacy download en
+#python -m spacy download en
 
 echo "spacy installed"
 
 # download the flaming-tyrion repo which houses the ontology XML files
 # this repo is updated weekly
 
-git -C /home/vagrant/shared/flaming-tyrion pull || git clone --depth=1 http://github.com/mrmechko/flaming-tyrion /home/vagrant/shared/flaming-tyrion
+#git -C /home/vagrant/shared/flaming-tyrion pull || git clone --depth=1 http://github.com/mrmechko/flaming-tyrion /home/vagrant/shared/flaming-tyrion
 
 echo "flaming-tyrion pulled"
 
